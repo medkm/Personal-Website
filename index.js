e607d739671680c126b0d3aaf60e914d6e94b37b
Mygel <mbergstresser@redventures.com>
2020-03-09 23:38:41 -0400

    Change default port

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ var path = require('path');
 //app.use(express.static(__dirname)); // Current directory is root
 app.use(express.static(path.join(__dirname, "src"))); //  "public" off of current is root
 
-app.listen(80);
-console.log("Listening on port 80");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
