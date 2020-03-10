e46dd0007406858c3a7ce50818393ead2d239dcc
Mygel <mbergstresser@redventures.com>
2020-03-09 23:12:54 -0400

    Convert single to double quotes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var path = require('path');
 
 //app.use(express.static(__dirname)); // Current directory is root
-app.use(express.static(path.join(__dirname, 'src'))); //  "public" off of current is root
+app.use(express.static(path.join(__dirname, "src"))); //  "public" off of current is root
 
 app.listen(80);
-console.log('Listening on port 80');
+console.log("Listening on port 80");
