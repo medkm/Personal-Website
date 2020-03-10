9dc13cae695e06d43a18987d5d3e26d5bb2c4185
Mygel <mbergstresser@redventures.com>
2020-03-09 22:50:26 -0400

    Converted code to express

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+var express = require('express');
+var app = express();
+var path = require('path');
 
-document.querySelector(".myButton").addEventListener("click", function(event) {
-    
-    var buttonInnerHTML = this.innerHTML;
-    alert("Kaleb Medlin" + "\n" + "Phone: (980) 581-1864" + "\n" + "kaleb.medlin@gmail.com");
-})
+//app.use(express.static(__dirname)); // Current directory is root
+app.use(express.static(path.join(__dirname, 'src'))); //  "public" off of current is root
+
+app.listen(80);
+console.log('Listening on port 80');
